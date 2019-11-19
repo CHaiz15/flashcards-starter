7b558742335e03fd8236289148efa2b457aa87a2
caleb.haizlett@gmail.com <alanturing@example.c>
2019-11-19 14:52:03 -0700

    Add initial setup of files

---
+++ src/Turn.js
@@ -0,0 +1,12 @@
+const card = require('./Card');
+
+class Turn {
+  cosntructor(guess, card) {
+    returnGuess();
+    returnCard();
+    evaluateGuess();
+    giveFeedback();
+  }
+}
+
+module.exports = Turn;
