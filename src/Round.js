08d90b4e8069bd5e340835d4c97b8799874ff2d7
caleb.haizlett@gmail.com <alanturing@example.c>
2019-11-20 15:32:57 -0700

    Add code to finish testing for round files

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,38 @@
+const Turn = require('../src/Turn');
+
 class Round {
   constructor(deck) {
     this.deck = deck.cards;
+    this.currentCard = this.deck[0];
+    this.turnsCount = 0;
+    this.incorrectGuesses = [];
+    this.correctGuesses = 0;
+    this.turn;
+    this.percentCorrect;
+    this.totalGuesses;
+  }
+  returnCurrentCard(){
+    return this.currentCard = this.deck[this.turnsCount];
+  }
+  takeTurn(guess){
+    const turn = new Turn(guess, this.deck[this.turnsCount]);
+    this.turn = turn;
+    turn.evaluateGuess();
+    if (!this.turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.deck[this.turnsCount].id);
+    } else if (this.turn.evaluateGuess()) {
+      this.correctGuesses++;
+    }
+    this.turnsCount++;
+    return this.turn.giveFeedback();
+  }
+  calculatePercentCorrect(){
+    this.totalGuesses = this.correctGuesses + this.incorrectGuesses.length;
+    this.percentCorrect = (this.correctGuesses / this.totalGuesses) * 100;
+    return this.percentCorrect;
+  }
+  endRound(){
+    return `** Round over! ** You answered ${this.percentCorrect}% of the questions correctly!`
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -31,6 +31,83 @@ it('should be an instance of the Round class', function() {
 
 it('should store a deck', function() {
   expect(round.deck).to.deep.equal([card1, card2, card3]);
-})
+});
+
+it('should return current card being played', function() {
+  expect(round.returnCurrentCard()).to.equal(deck.cards[0]);
+});
+
+it('should have a default value of 0 for turnsCount', function() {
+  expect(round.turnsCount).to.equal(0);
+});
+
+it('should update turnsCount after a turn is taken', function() {
+  round.takeTurn('capybara');
+  expect(round.turnsCount).to.equal(1);
+  round.takeTurn('capybara');
+  round.takeTurn('capybara');
+  expect(round.turnsCount).to.equal(3);
+});
+
+it('should have a default value for turn', function() {
+  expect(round.turn).to.equal(undefined);
+});
+
+it('should instantiate a new Turn when a guess is made', function() {
+  round.takeTurn('capybara');
+  expect(round.turn).to.be.an.instanceof(Turn);
+});
+
+it('should advance one card each turn', function() {
+  round.takeTurn('capybara');
+  expect(round.turnsCount).to.equal(1);
+  expect(round.deck[round.turnsCount]);
+});
+
+it('should have a default value for incorrectGuesses', function() {
+  expect(round.incorrectGuesses).to.deep.equal([]);
+});
+
+it('should evaluate guesses', function() {
+  round.takeTurn('sea otter');
+  expect(round.turn.evaluateGuess()).to.equal(true);
+
+  round.takeTurn('wrong');
+  expect(round.turn.evaluateGuess()).to.equal(false);
+});
+
+it('should give feedback for correct or incorrect guess', function() {
+  round.takeTurn('sea otter');
+  expect(round.turn.giveFeedback()).to.equal('Correct!');
+
+  round.takeTurn('wrong');
+  expect(round.turn.giveFeedback()).to.equal('Incorrect!');
+});
+
+it('should store the ids of incorrectGuesses', function() {
+  round.takeTurn('pug');
+  expect(round.incorrectGuesses).to.deep.equal([1])
+});
+
+it('should increment correctGuesses by 1 when a guess is correct', function() {
+  round.takeTurn('sea otter');
+  expect(round.correctGuesses).to.equal(1);
+});
+
+it('should calculate the percentage of correct guesses', function() {
+  round.takeTurn('pug');
+  round.takeTurn('gallbladder');
+  round.calculatePercentCorrect();
+
+  expect(round.percentCorrect).to.equal(50);
+});
+
+it('should print percent of correct guesses', function () {
+  round.takeTurn('pug');
+  round.takeTurn('gallbladder');
+  round.calculatePercentCorrect();
+
+  expect(round.endRound()).to.equal('** Round over! ** You answered 50% of the questions correctly!');
+});
 
 });
